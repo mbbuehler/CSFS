758ea25980b9c30d3d31ef4a290a6bc8cfb14d7b
Marcello <marcel.buehler@uzh.ch>
2016-10-10 16:23:06 +0200

    analysis olympia std and creation of artifical 10

--- analysis_olympia.py
+++ analysis_olympia.py
@@ -69,5 +69,84 @@ def analysis_std():
 
     sys.stdout.write('analysis with std ranging from {} to {} is finished.'.format(start_std, end_std))
 
+def get_result_data(n_features):
+    """
 
-analysis_std()
+    :return: {no_features: {std: auc},...} e.g. {16: {0.200036667: 0.53119531952662713, 0.105176567: 0.57273262130177505
+    """
+    path = 'pickle-dumps/olympia/'
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))]
+
+    results = dict()
+    pattern = r'(\d+)features_100samples_(.*?)std'
+    for f in files:
+        match = re.match(pattern, f)
+        # print(f)
+        no_features = int(match.group(1))
+        std = float(match.group(2))
+
+        if no_features not in results.keys():
+            results[no_features] = dict()
+
+        results[no_features][std] = (np.mean(pickle.load(open(os.path.join(path,f), 'rb'))['best_noisy']))
+    if n_features:
+        results = {r:results[r] for r in n_features}
+    return results
+
+def extract_x_y(result, n_features, start_lim=0):
+    """
+    extracts x and y from results for a certain n_features
+    :param result:
+    :param n_features:
+    :return: x,y where x: std and y:auc
+    """
+    if n_features not in result.keys():
+        print('{} not found'.format(n_features))
+        return None
+    x = sorted([std for std in result[n_features].keys() if std > start_lim])
+    y = [result[n_features][std] for std in x]
+
+    return np.array(x, dtype=float), np.array(y, dtype=float)
+
+def visualize_result():
+    dataset_name = "olympia"
+    target = 'target'
+    N_features = [3,5,7]#,11,13,16]
+    N_samples = 100
+    results = get_result_data(N_features)
+    plt.hold(True)
+    start_lim = 0.1
+    params = dict()
+
+    def func(x,  w1, p1, w2, p2):
+        return w1 * pow(x, p1) + w2 * pow(x, p2)
+
+    # def func(x, w1, p1, w2, p2, w3):
+    #     return w1 * pow(x, p1) + w2 * pow(x, p2) + w3 * np.log10(x)
+
+    for n_f in N_features:
+        print('== no of features: {}'.format(n_f))
+        x,y = extract_x_y(results, n_f, start_lim=0)
+        std = np.std(y)
+        plt.plot(x, y, alpha=0.5, label='data {} (std={:.3f})'.format(n_f, std))
+        x,y = extract_x_y(results, n_f, start_lim=start_lim)
+        popt, pcov = curve_fit(func, x, y)
+        params[n_f] = popt
+        perr = np.sqrt(np.diag(pcov))
+        avg_err = np.mean(perr)
+        print('params: {} '.format(popt))
+        print('errors: {}'.format(perr))
+        print('avg error: {}'.format(avg_err))
+
+        plt.plot(x, func(x, *popt), '-k', linewidth=2, label="Fitted {} (avg err: {:.3f})".format(n_f, avg_err))
+
+    plt.legend(loc=3)
+    plt.title('auc scores / fitted curves for noisy IG. start fitting at std={}'.format(start_lim))
+    plt.xlim([-.01, 0.31])
+    plt.xlabel('std')
+    plt.ylabel('auc')
+    fig1 = plt.gcf()
+    plt.show()
+    fig1.savefig('plots/olympia/std_result.png', dpi=100)
+
+visualize_result()
--- create_dataset.py
+++ create_dataset.py
@@ -1,72 +1,159 @@
 import numpy as np
 import pandas as pd
-from numpy.random import random
+from math import exp
+
 import numpy as np
-from sklearn.preprocessing import binarize
 import pickle
+import matplotlib.pyplot as plt
 
 dump_path = 'datasets/artificial/'
 
+
 def artifical1():
-     N_features = 100
-     N_samples = 1000
+    N_features = 100
+    N_samples = 1000
 
-     def get_bin_random_col(mean, std, n, t):
-          values = np.random.normal(mean, std, n)
-          values = [0 if v < t else 1 for v in values]
-          return values
+    def get_bin_random_col(mean, std, n, t):
+        values = np.random.normal(mean, std, n)
+        values = [0 if v < t else 1 for v in values]
+        return values
 
-     means = random(N_features)
-     stds = random(N_features)/3
-     samples = pd.DataFrame()
-     for i in range(N_features):
-          t = .5
-          samples['X{}'.format(i)] = get_bin_random_col(means[i], stds[i], N_samples, t)
-     samples['T'] = get_bin_random_col(0.5, 0.2, N_samples, 0.5)
+    means = random(N_features)
+    stds = random(N_features) / 3
+    samples = pd.DataFrame()
+    for i in range(N_features):
+        t = .5
+        samples['X{}'.format(i)] = get_bin_random_col(means[i], stds[i], N_samples, t)
+    samples['T'] = get_bin_random_col(0.5, 0.2, N_samples, 0.5)
 
-     print(samples.describe())
-     art1_path = dump_path+'/artificial1.pickle'
-     pickle.dump(samples, open(art1_path, 'wb'))
+    print(samples.describe())
+    art1_path = dump_path + '/artificial1.pickle'
+    pickle.dump(samples, open(art1_path, 'wb'))
+
 
 def artifical2():
-     N_features = 9
-     N_samples = 1000
+    N_features = 9
+    N_samples = 1000
 
-     def get_bin_random_col(mean, std, n, t):
-          values = np.random.normal(mean, std, n)
-          values = [0 if v < t else 1 for v in values]
-          return values
+    def get_bin_random_col(mean, std, n, t):
+        values = np.random.normal(mean, std, n)
+        values = [0 if v < t else 1 for v in values]
+        return values
 
-     means = random(N_features)
-     stds = random(N_features)/3
-     samples = pd.DataFrame()
-     for i in range(N_features):
-          t = .5
-          samples['X{}'.format(i)] = get_bin_random_col(means[i], stds[i], N_samples, t)
-     samples['T'] = get_bin_random_col(0.5, 0.2, N_samples, 0.5)
+    means = random(N_features)
+    stds = random(N_features) / 3
+    samples = pd.DataFrame()
+    for i in range(N_features):
+        t = .5
+        samples['X{}'.format(i)] = get_bin_random_col(means[i], stds[i], N_samples, t)
+    samples['T'] = get_bin_random_col(0.5, 0.2, N_samples, 0.5)
 
-     print(samples.describe())
-     art2_path = dump_path+'/artificial2.pickle'
-     pickle.dump(samples, open(art2_path, 'wb'))
+    print(samples.describe())
+    art2_path = dump_path + '/artificial2.pickle'
+    pickle.dump(samples, open(art2_path, 'wb'))
+
 
 def artifical3():
-     N_features = 20
-     N_samples = 1000
+    N_features = 20
+    N_samples = 1000
 
-     def get_bin_random_col(mean, std, n, t):
-          values = np.random.normal(mean, std, n)
-          values = [0 if v < t else 1 for v in values]
-          return values
+    def get_bin_random_col(mean, std, n, t):
+        values = np.random.normal(mean, std, n)
+        values = [0 if v < t else 1 for v in values]
+        return values
 
-     means = random(N_features)
-     stds = random(N_features)/3
-     samples = pd.DataFrame()
-     for i in range(N_features):
-          t = .5
-          samples['X{}'.format(i)] = get_bin_random_col(means[i], stds[i], N_samples, t)
-     samples['T'] = get_bin_random_col(0.5, 0.2, N_samples, 0.5)
+    means = random(N_features)
+    stds = random(N_features) / 3
+    samples = pd.DataFrame()
+    for i in range(N_features):
+        t = .5
+        samples['X{}'.format(i)] = get_bin_random_col(means[i], stds[i], N_samples, t)
+    samples['T'] = get_bin_random_col(0.5, 0.2, N_samples, 0.5)
 
-     print(samples.describe())
-     art3_path = dump_path+'/artificial3.pickle'
-     pickle.dump(samples, open(art3_path, 'wb'))
-artifical2()
+    print(samples.describe())
+    art3_path = dump_path + '/artificial3.pickle'
+    pickle.dump(samples, open(art3_path, 'wb'))
+
+
+def create_artifical(dataset_name, N_features, N_samples, std):
+
+    def sigmoid_correct(x, params):
+        """
+
+        :param x: list of (binary) features [0, 0, 0, 1, 1]
+        :param params: list of params [-1, 2, 10, -5,...]
+        :return:
+        """
+        assert len(x) == len(params)
+        alpha = 1
+        return alpha / (1 + np.exp(-(sum([params[i] * x[i] for i in range(len(params))]))))  # + w2*x2)))
+
+    def sigmoid_noisy(x, params, std):
+        y_actual = sigmoid_correct(x, params)
+        y_noisy = np.random.normal(y_actual, std)
+        return y_noisy
+    def binarize(array_data, threshold=0.5):
+        return [1 if a >= threshold else 0 for a in array_data]
+
+
+    relevant_params = [-10, -5, 1, .5, 50]
+    zero_params = [0] * (N_features - len(relevant_params))
+
+    params = relevant_params + zero_params
+    np.random.seed()
+    random = np.random.random
+    X = [binarize([random() for i in range(N_features)]) for j in range(N_samples)]
+    y = binarize([sigmoid_correct(X[i], params) for i in range(N_samples)])
+    y_noisy = binarize([sigmoid_noisy(X[i], params, std) for i in range(N_samples)])
+    # print(y)
+    # print(y_noisy)
+
+
+    file_base_path = 'datasets/artificial/'
+    col_names = ['F{}_{}'.format(i, params[i]) for i in range(len(params))]
+    col_names.append('T')
+    csv_path = file_base_path+dataset_name+'.csv'
+    write_csv(csv_path, X, y, col_names)
+    readme_path = file_base_path+dataset_name+'.md'
+    write_readme(readme_path, X, y, y_noisy, std, len(relevant_params), params)
+
+
+def write_csv(file_path, X, y, col_names):
+    import csv
+    file = open(file_path, 'w', newline="")
+    writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
+    data = [col_names]
+    data += [X[i]+[y[i]] for i in range(len(y))]
+    print(data)
+    writer.writerows(data)
+    file.close()
+
+def write_readme(file_path, X, y, y_noisy, std, n_relevant_features, params):
+    file = open(file_path, 'w')
+
+    unequal = sum([1 if y[i] != y_noisy[i] else 0 for i in range(len(y))])
+    unequal_ratio = unequal/len(y)
+
+    info = (
+        ('N_samples', len(X)),
+        ('N_features', len(X[0])),
+        ('N_relevant features', n_relevant_features),
+        ('STD for noise generation', std),
+        ('Noisy y (actual / ratio)', "{} / {}".format(unequal, unequal_ratio)),
+        ('Params', ",".join([str(p) for p in params])),
+    )
+    for a,b in info:
+        file.write('{:<30}: {}\n'.format(a,b))
+    file.close()
+
+def main():
+    dataset_name = "artificial10"
+    N_features = 50
+    N_samples = 1000
+    std = 0.35
+
+    create_artifical(dataset_name, N_features, N_samples, std)
+
+
+if __name__ == "__main__":
+    main()
