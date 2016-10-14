5c52c06e9882129a9eeea8c0cda7b0c3562bce6c
Marcello <marcel.buehler@uzh.ch>
2016-10-14 10:52:02 +0200

    prepared analysis 3x and 4x

--- analysis_artificial3x.py
+++ analysis_artificial3x.py
@@ -3,21 +3,19 @@ from joblib import Parallel, delayed
 from analysis_artificial import analysis_general, visualise_results
 
 
+dataset_names = ['artificial30','artificial31','artificial32','artificial33','artificial34','artificial35']
+
 def do_analysis():
     N_features = [3,5,7,10]#,11,13,16]
     N_samples = 100
-    analysis_general("artificial30",N_features, N_samples)
-    analysis_general("artificial31",N_features, N_samples)
-    analysis_general("artificial32",N_features, N_samples)
-    analysis_general("artificial33",N_features, N_samples)
-    analysis_general("artificial34",N_features, N_samples)
+    for dn in dataset_names:
+        analysis_general(dn, N_features, N_samples)
 
 def evaluate():
     N_features = [3,5,7,10]
-    dataset_names = ['artificial30','artificial31','artificial32','artificial33','artificial34']
     Parallel(n_jobs=4)(delayed(visualise_results)(dn, N_features, start_lim=0) for dn in dataset_names)
     # N: #data points
     # M: #parameters
 
-# do_analysis()
+do_analysis()
 evaluate()
--- analysis_artificial4x.py
+++ analysis_artificial4x.py
@@ -4,21 +4,19 @@ from joblib import Parallel, delayed
 from analysis_artificial import analysis_general, visualise_results
 
 
+dataset_names = ['artificial40','artificial41','artificial42','artificial43','artificial44']
+
 def do_analysis():
     N_features = [3,5,7,10]#,11,13,16]
     N_samples = 100
-    analysis_general("artificial40",N_features, N_samples)
-    analysis_general("artificial41",N_features, N_samples)
-    analysis_general("artificial42",N_features, N_samples)
-    analysis_general("artificial43",N_features, N_samples)
-    analysis_general("artificial44",N_features, N_samples)
+    for dn in dataset_names:
+        analysis_general(dn, N_features, N_samples)
 
 def evaluate():
     N_features = [3,5,7,10]
-    dataset_names = ['artificial40','artificial41','artificial42','artificial43','artificial44']
     Parallel(n_jobs=4)(delayed(visualise_results)(dn, N_features, start_lim=0.035) for dn in dataset_names)
     # N: #data points
     # M: #parameters
 
-# do_analysis()
+do_analysis()
 evaluate()
