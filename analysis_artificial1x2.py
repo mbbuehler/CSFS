cdb75359179d0d5a83de82da18851f01b73b9a73
Marcello <marcel.buehler@uzh.ch>
2016-10-22 12:38:41 +0200

    adjusted naming of output for plots

--- analysis_artificial1x2.py
+++ analysis_artificial1x2.py
@@ -7,7 +7,7 @@ from analysis_artificial import analysis_general, visualise_results
 N_features = [3,5,7,10]
 dataset_base = ['artificial10_2','artificial11_2','artificial12_2','artificial13_2','artificial14_2']
 dataset_names = [dn+'_true' for dn in dataset_base] + [dn+'_noisy' for dn in dataset_base]
-# dataset_names = ['artificial10_2_true']
+dataset_names = ['artificial10_2_true']
 
 def do_analysis():
     N_samples = 100
@@ -19,5 +19,5 @@ def evaluate():
     # N: #data points
     # M: #parameters
 
-do_analysis()
-# evaluate()
+# do_analysis()
+evaluate()
