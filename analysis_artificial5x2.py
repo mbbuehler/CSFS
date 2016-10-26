c8a079a803c932f559152b6d9f1e1fc713c00f42
Marcello <marcel.buehler@uzh.ch>
2016-10-26 11:24:44 +0200

    prepared analysis 5x2 and 6x2

--- analysis_artificial5x2.py
+++ analysis_artificial5x2.py
@@ -3,10 +3,11 @@ from joblib import Parallel, delayed
 
 from analysis_artificial import analysis_general, visualise_results, create_dn_names_from_base
 
-
 N_features = [10, 20, 35, 50, 70, 90]
-dataset_names = ['artificial6{}'.format(i) for i in range(0,10)]
-# dataset_names = ['artificial69']
+
+dataset_base = ['artificial5{}_2'.format(i) for i in range(0,10)]
+dataset_names = create_dn_names_from_base(dataset_base)
+
 
 def do_analysis():
     N_samples = 100
@@ -18,5 +19,5 @@ def evaluate():
     # N: #data points
     # M: #parameters
 
-# do_analysis()
-evaluate()
+do_analysis()
+# evaluate()
