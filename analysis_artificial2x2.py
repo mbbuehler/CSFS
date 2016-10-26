2bdeb3f182d1308f1aa035115fec3f2a1a1a35f1
Marcello <marcel.buehler@uzh.ch>
2016-10-26 11:06:37 +0200

    finishing analysis 2x2

--- analysis_artificial2x2.py
+++ analysis_artificial2x2.py
@@ -6,9 +6,9 @@ from analysis_artificial import analysis_general, visualise_results
 
 N_features = [3,5,7,10]
 dataset_base = ['artificial20_2','artificial21_2','artificial22_2','artificial23_2','artificial24_2','artificial25_2','artificial26_2']
-dataset_base = ['artificial24_2','artificial25_2','artificial26_2']
+# dataset_base = ['artificial24_2','artificial25_2','artificial26_2']
 dataset_names = [dn+'_true' for dn in dataset_base] + [dn+'_noisy' for dn in dataset_base]
-dataset_base = ['artificial20_2_noisy','artificial21_2_noisy','artificial22_2_noisy', 'artificial23_2_noisy']
+dataset_names = ['artificial20_2_noisy','artificial21_2_noisy','artificial22_2_noisy', 'artificial23_2_noisy']
 # dataset_names = ['artificial10_2_true']
 
 def do_analysis():
