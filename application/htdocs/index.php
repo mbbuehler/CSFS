6050c3f45c05375d8bd21b391a1369df692ee8b8
Marcello <marcel.buehler@uzh.ch>
2016-12-09 13:50:11 +0100

    fixing error

--- application/htdocs/index.php
+++ application/htdocs/index.php
@@ -24,7 +24,7 @@
             crossorigin="anonymous"></script>
     <script src="circle.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="style.css"/>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
 
 
 </head>
