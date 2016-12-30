eaa0bdeabd51d91af969691db8d68752cb42f7af
Marcello <marcel.buehler@uzh.ch>
2016-12-30 07:42:40 +0100

    paths and evaluation

--- experiment_income.py
+++ experiment_income.py
@@ -15,13 +15,13 @@ class ExperimentIncome(AbstractExperiment):
         self.path_cleaned = '{}cleaned/{}/income_clean.csv'.format(self.base_path, experiment_name)
         self.path_bin = '{}cleaned/{}/income_clean_bin.csv'.format(self.base_path, experiment_name)
         self.path_meta = '{}cleaned/{}/income_clean_bin_meta.csv'.format(self.base_path, experiment_name)
-        # self.path_answers_raw = '{}results/{}/answers_raw.xlsx'.format(self.base_path, experiment_name)
-        # self.path_answers_clean = '{}results/{}/answers_clean.csv'.format(self.base_path, experiment_name)
-        # self.path_answers_aggregated = '{}results/{}/answers_aggregated.csv'.format(self.base_path, experiment_name)
-        # self.path_answers_metadata = '{}results/{}/answers_metadata.csv'.format(self.base_path, experiment_name)
+        self.path_answers_raw = '{}results/{}/answers_raw.xlsx'.format(self.base_path, experiment_name)
+        self.path_answers_clean = '{}results/{}/answers_clean.csv'.format(self.base_path, experiment_name)
+        self.path_answers_aggregated = '{}results/{}/answers_aggregated.csv'.format(self.base_path, experiment_name)
+        self.path_answers_metadata = '{}results/{}/answers_metadata.csv'.format(self.base_path, experiment_name)
         # self.path_csfs_auc = '{}results/{}/csfs_auc.csv'.format(self.base_path, experiment_name)
         # self.path_csfs_std = '{}results/{}/csfs_std.csv'.format(self.base_path, experiment_name)
-        # self.path_questions = '{}questions/{}/questions.csv'.format(self.base_path, experiment_name) # experiment2 for experiment3
+        self.path_questions = '{}questions/{}/questions.csv'.format(self.base_path, experiment_name)
         self.path_flock_result = '{}results/{}/flock_auc.csv'.format(self.base_path, experiment_name)
         #
         # self.path_cost_ig_test = 'application/conditions/test/income.csv'
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     # experiment.preprocess_raw()
     # experiment.bin_binarise()
     # experiment.get_metadata()
-    # experiment.evaluate_crowd_all_answers()
+    experiment.evaluate_crowd_all_answers()
      # experiment.drop_analysis(N_Features, n_samples)
     # experiment.evaluate_flock(N_Features, n_samples, range(3, 100, 1))
     # experiment.evaluate_csfs_auc(fake_features={'G3': 0.5})
