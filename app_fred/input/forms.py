fde7c462448390f22bf82b012290adedb567d162
Marcello <marcel.buehler@uzh.ch>
2017-02-23 08:35:02 +0100

    models and csv

--- app_fred/input/forms.py
+++ app_fred/input/forms.py
@@ -1,10 +1,69 @@
 
 from django import forms
-
+import csv
+from django.core.exceptions import ValidationError
 
 class NewJobForm(forms.Form):
     email = forms.EmailField(label='E-mail')
     amt_key = forms.CharField(label='AMT key', max_length=21, min_length=20)
-    features_csv = forms.FileField(label='CSV file')
+    features_csv = CSVField(label='CSV file', expected_fieldnames=['Feature',
+                                                                   'Question P(X|Y=0)',
+                                                                   'Question P(X|Y=1)',
+                                                                   'Question P(X)',
+                                                                   'P(X|Y=0)',
+                                                                   'P(X|Y=1)',
+                                                                   'P(X)'
+                                                                   ]
+                            )
     target_mean_checkbox = forms.BooleanField(label='Query the target mean', initial=True, required=False)
     target_mean = forms.FloatField(label='Target mean', initial=0.5, required=False)
+    target_mean_question = forms.CharField(label='Question for target mean (P(Y))')
+
+
+
+
+
+
+class CSVField(forms.FileField):
+    """
+    FileField that checks that the file is a valid CSV and if specified
+    in `expected_fieldnames` checks that the fields match exactly.
+
+    The widget's `accept` parameter is set to accept csv, text and excel files.
+
+    TODO: Validate the entirety of the CSV file, not just the headers.
+          But this should be enough for most use cases, as checking the
+          whole file could be computationally expensive for huge files.
+
+    source: https://djangosnippets.org/snippets/10596/
+
+    Example usage:
+        people = CSVField(expected_fieldnames=['First Name', 'Last Name'])
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.expected_fieldnames = kwargs.pop('expected_fieldnames', None)
+        super(CSVField, self).__init__(*args, **kwargs)
+        self.error_messages['required'] = 'You must select a file'
+        self.widget.attrs.update(
+            {'accept': '.csv,'
+                       'text/plain,'
+                       'application/vnd.ms-excel,'
+                       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'})
+
+    def clean(self, data, initial=None):
+        value = super(CSVField, self).clean(data)
+        reader = csv.reader(data)
+        # Check it's a valid CSV file
+        try:
+            fieldnames = reader.next()
+        except csv.Error:
+            raise ValidationError('You must upload a valid CSV file')
+
+        # Check the fieldnames are as specified, if requested
+        if self.expected_fieldnames and fieldnames != self.expected_fieldnames:
+            raise ValidationError(
+                u'The CSV fields are expected to be "{0}"'.format(
+                    u','.join(self.expected_fieldnames)))
+
+        return value
--- app_fred/input/models.py
+++ app_fred/input/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Job(models):
+
+    class Status:
+        CREATED = 'created'
+        STARTED = 'started'
+        PROCESSING = 'processing'
+        FINISHED = 'finished'
+        FAILED = 'failed'
+
+    email = models.EmailField()
+    amt_key = models.TextField()
+    query_target_mean = models.BooleanField()
+    target_mean = models.FloatField()
+    target_question = models.TextField()
+    status = models.TextField(default=Status.CREATED)
+    date_created = models.DateField(default=timezone.now)
+    date_started = models.DateField(blank=True)
+    date_finished =models.DateField(blank=True)
+
+class Feature(models):
+    name = models.CharField()
+    job = models.ForeignKey( # a feature belongs to one job, a job can have M features
+        Job,
+        on_delete=models.CASCADE,
+    )
+    q_p_0 = models.CharField() # Question for P(X|Y=0)
+    q_p_1 = models.CharField()
+    q_p = models.CharField()
+    p_0 = models.FloatField() # P(X|Y=0)
+    p_1 = models.FloatField()
+    p = models.FloatField()
+    ig = models.FloatField()
+
+
+
