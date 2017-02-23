49fe0b84ed30e1ffc66d089ecf0a1a3452b11d15
Marcello <marcel.buehler@uzh.ch>
2017-02-23 18:56:32 +0100

    finished screenshots

--- app_fred/input/forms.py
+++ app_fred/input/forms.py
@@ -65,7 +65,7 @@ class NewJobForm(forms.Form):
     target_mean_checkbox = forms.BooleanField(label='Query the target mean', initial=True, required=False)
     target_mean = forms.FloatField(label='Target mean', initial=0.5, required=False)
     target_mean_question = forms.CharField(label='Question for target mean (P(Y))', required=False)
-    job_id = forms.IntegerField(widget=forms.HiddenInput(), required=False)
+    job_id = forms.IntegerField(widget=forms.HiddenInput(), required=False, label="")
 
 
 
